1f13b8b695ef7bcd251bd1920903f4de4de71a2f
Johnny Gérard <john@jgerard.dev>
2025-06-08 23:34:25 +0200

    refactor: set default font-size

--- src/app/component/link.ts
+++ src/app/component/link.ts
@@ -7,7 +7,7 @@ import { RouterLink } from "@angular/router";
   imports: [ArrowRightLine, RouterLink],
   template: `
     <a
-      class="flex w-fit items-center gap-1.5 rounded-sm text-base font-medium text-indigo-700 ring-[rgba(68,76,231,0.12)] outline-none hover:text-indigo-800 focus-visible:text-indigo-800 focus-visible:ring-4"
+      class="flex w-fit items-center gap-1.5 rounded-sm font-medium text-indigo-700 ring-[rgba(68,76,231,0.12)] outline-none hover:text-indigo-800 focus-visible:text-indigo-800 focus-visible:ring-4"
       [routerLink]="url()"
     >
       <span class="px-0.5">
