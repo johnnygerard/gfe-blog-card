4cc63221541055129793c352cd5b34148be99478
Johnny Gérard <john@jgerard.dev>
2025-06-09 03:18:58 +0200

    refactor: use outline instead of ring

--- src/app/component/link.ts
+++ src/app/component/link.ts
@@ -7,7 +7,7 @@ import { RouterLink } from "@angular/router";
   imports: [ArrowRightLine, RouterLink],
   template: `
     <a
-      class="flex w-fit items-center gap-1.5 rounded-sm font-medium text-indigo-700 ring-[rgba(68,76,231,0.12)] outline-none hover:text-indigo-800 focus-visible:text-indigo-800 focus-visible:ring-4"
+      class="flex w-fit items-center gap-1.5 rounded-sm font-medium text-indigo-700 outline-4 outline-transparent transition-colors hover:text-indigo-800 focus-visible:text-indigo-800 focus-visible:outline-[rgba(68,76,231,0.12)]"
       [routerLink]="url()"
     >
       <span class="px-0.5">
